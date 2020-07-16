a5a87d960cfedc6d3d049ed0e4c627d1127f59cf
Rohit Khadatkar <rohit@catapulthq.com>
2020-07-16 23:08:52 +0530

    Add category endPoints

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const CONNECTION_URL = '';
 const DATABASE_NAME = "project";
 var mongoose = require('mongoose')
 
-var mongodb = 'mongodb://127.0.0.1:27017/project';
-mongoose.connect(mongodb);
+var mongodb_url = 'mongodb://127.0.0.1:27017/';
+// mongoose.connect(mongodb);
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-var db = mongoose.connection;
+// var db = mongoose.connection;
 
 const app = express();
 
@@ -29,17 +29,41 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+// server connection and database connection
 app.listen(3000, function () {
+  console.log('Server is running on port 3000 !!!');  
+});
 
-  console.log('Server is running on port 3000 !!!');
-  
-  db.on('connected', function(){
-      console.log("Database connected successfully !!! ")
+
+// db.on('connected', function(){
+//     console.log("Database connected successfully !!! ")
+// })
+
+// db.on('error', function(err){
+//   console.log("Database connection error: " + err)
+//  }) 
+
+MongoClient.connect(mongodb_url, (err, client) => {
+
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+    const db = client.db('project')
+
+    // Add category endpoints
+    app.post("/add_category", (req, res) => {
+        console.log("In Category Method : ", req.body);       
+        db.collection('category_master').insertMany(req.body, (error, result) => {
+            if(error) {
+                console.log(error);
+                return res.status(500).send(error);
+            }
+            res.send(result.result);
+            console.log("Category Added");
+        });
+    });
+
+
   })
 
-  db.on('error', function(err){
-    console.log("Database connection error: " + err)
-   })
 
- });
 
